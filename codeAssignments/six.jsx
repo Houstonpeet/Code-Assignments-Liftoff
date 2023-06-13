77893ba5c24a36248a3fe885cc8d1223d8efbf42
Houstonpeet <Peethouston@gmail.com>
2023-06-12 19:47:32 -0500

    assignment 7

---
+++ codeAssignments/six.jsx
@@ -0,0 +1,13 @@
+function detectWord(crowd) {
+    let word = '';
+  
+    for (let i = 0; i < crowd.length; i++) {
+      if (crowd[i] === crowd[i].toLowerCase()) {
+        word += crowd[i];
+      }
+    }
+  
+    return word;
+  }
+
+console.log(detectWord('KSHIhSEOJeDKSFIlLSHGlLSEFJoEHF'))
