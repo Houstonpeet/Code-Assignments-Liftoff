37e66f012d37ecf6ba65b97201ade58b3b5d9afd
Houstonpeet <Peethouston@gmail.com>
2023-06-14 16:33:13 -0500

    Code assignment 8

---
+++ codeAssignments/Eight.jsx
@@ -0,0 +1,10 @@
+function countOccurrences(char, string) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      count++;
+    }
+  }
+  return count;
+}
+
